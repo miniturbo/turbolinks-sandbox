975152e920c9c8eb01b238809a2ffcf13dda9a52
Kazuya Shono <kazuya.shono@gmail.com>
2017-09-06 00:55:24 +0900

    fix

--- app/assets/javascripts/root.js
+++ app/assets/javascripts/root.js
@@ -2,11 +2,21 @@
 
 document.addEventListener('DOMContentLoaded', function() {
   console.log('DOM content loaded.')
-  Array.from(document.querySelectorAll('.invoker')).forEach(function(elem) {
-    elem.addEventListener('click', function() {
+  // Array.from(document.querySelectorAll('.invoker')).forEach(function(elem) {
+  //   elem.addEventListener('click', function() {
+  //     console.log('Invoked.');
+  //     alert('Invoked.');
+  //   });
+  // });
+  document.addEventListener('click', function(e) {
+    var elem = closest(e.target, '.invoker');
+    if (elem) {
+      console.log('Closest invoker element:', elem);
       console.log('Invoked.');
       alert('Invoked.');
-    });
+    } else {
+      console.log('Closest invoker element not found.');
+    }
   });
 });
 
--- app/assets/javascripts/utils.js
+++ app/assets/javascripts/utils.js
@@ -5,3 +5,24 @@ function isSupported() {
     return false;
   }
 }
+
+function matches(element, selector) {
+  var elements = (element.document || element.ownerDocument).querySelectorAll(selector);
+
+  var index = 0;
+  while (elements[index] && elements[index] !== element) {
+    ++index;
+  }
+
+  return Boolean(elements[index]);
+}
+
+function closest(element, selector) {
+  while (element && element.nodeType === 1) {
+    if (matches(element, selector)) {
+      return element;
+    }
+    element = element.parentNode;
+  }
+  return null;
+}
